a94a0a72582f3a321d92d7f0f2428ea1ff91c7a5
tanahara <s10.vermouth.4869@gmail.com>
2020-08-10 15:18:22 +0900

    マイグレーションを実行

--- db/migrate/20200810055617_create_posts.rb
+++ db/migrate/20200810055617_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :content
       t.string :images
       t.integer :position
-      t.boolean :random
+      t.boolean :random, default: false, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
