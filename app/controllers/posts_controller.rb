451f0d20adcf4d7013918ddd755c89aff9ad80ab
tanahara <s10.vermouth.4869@gmail.com>
2020-08-12 16:56:19 +0900

    画像を複数投稿できるよう実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, { images: [] })
   end
 
   def correct_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  mount_uploaders :images, ImageUploader
 end
