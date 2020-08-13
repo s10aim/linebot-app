1910d53832d25a5b4090bf2a571e854e147d7816
tanahara <s10.vermouth.4869@gmail.com>
2020-08-13 09:20:43 +0900

    画像ファイルの加工とバリデーションを追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+  process resize_to_limit: [280, 380]
   #
   # def scale(width, height)
   #   # do something
@@ -35,13 +36,19 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  # process convert: "jpg"
+
+  def filename
+    super.chomp(File.extname(super)) + ".jpg" if original_filename.present?
+  end
+
+  def size_range
+    0..30.megabytes
+  end
 end
