87b93df1f33dcb555158305f22ea79ab85567598
tanahara <s10.vermouth.4869@gmail.com>
2020-08-22 16:22:56 +0900

    usersのdeviseに関するカラムを変更

---
+++ db/migrate/20200822063154_remove_columns_from_users.rb
@@ -0,0 +1,8 @@
+class RemoveColumnsFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_index :users, :reset_password_token
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+  end
+end
