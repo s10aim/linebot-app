1616e8c8ef4498fc1a1486bd67a418a87e8a6d0f
tanahara <s10.vermouth.4869@gmail.com>
2020-09-10 20:46:55 +0900

    空投稿ができないようにrailsでバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploaders :images, ImageUploader
+
+  validates :title, presence: true
+  validate :confirmation_post_form
+
+  def confirmation_post_form
+    errors.add(:base, "画像または本文が必要です") unless content.present? ^ images?
+  end
 end
