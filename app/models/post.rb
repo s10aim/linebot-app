214d0e7c1bc9e77048b479d0bba0999216c9ffe5
tanahara <s10.vermouth.4869@gmail.com>
2020-09-18 18:12:51 +0900

    postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   mount_uploaders :images, ImageUploader
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :user_id }
   validate :confirmation_post_form
 
   def confirmation_post_form
