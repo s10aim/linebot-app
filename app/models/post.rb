07052ab696680b4ba459b330bfba43c7ba204209
tanahara <s10.vermouth.4869@gmail.com>
2020-09-22 21:54:33 +0900

    postモデルにタイトルのバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :user_id }
   validate :confirmation_post_form
   validate :post_random_images
+  validate :confirmation_keywords_list
 
   def confirmation_post_form
     errors.add(:base, "画像または本文が必要です") unless content.present? ^ images?
@@ -15,6 +16,12 @@ class Post < ApplicationRecord
   def post_random_images
     if images.length > 5 && !random
       errors.add(:base, "全ての画像を送信する場合は5枚以下にして下さい")
+    end
+  end
+
+  def confirmation_keywords_list
+    if KEYWORDS_LIST.include?(title)
+      errors.add(:base, "タイトルは「#{KEYWORDS_LIST.join(",")}」以外で入力して下さい")
     end
   end
 end
